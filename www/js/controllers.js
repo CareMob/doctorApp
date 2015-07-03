c91230aa627e0d69e7f74787e1e5ae62951edc41
mtfaoro <mtfaoro@gmail.com>
2015-07-02 22:18:58 -0300

    Ajuste cores
    Ajuste cores

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -15,8 +15,9 @@ angular.module('starter.controllers', [])
   // Create the login modal that we will use later
   $ionicModal.fromTemplateUrl('templates/login.html', {
     scope: $scope
-  }).then(function(modal) {
-    $scope.modal = modal;
+  }).then(function($ionicModal) {
+    $scope.modal = $ionicModal;
+    
   });
 
   // Triggered in the login modal to close it
@@ -31,7 +32,7 @@ angular.module('starter.controllers', [])
 
   // Perform the login action when the user submits the login form
   $scope.doLogin = function() {
-    console.log('Doing login', $scope.loginData);
+    
 
     // Simulate a login delay. Remove this and replace with your login
     // code if using a login system
