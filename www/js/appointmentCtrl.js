d569295ce324876f7e0cbce4acc60fcea8af8139
Marcelo Menegat <menegat.marcelo@gmail.com>
2015-07-07 23:07:45 -0300

    Ajustes Nova consulta
    Ajustes na tela de nova consulta.

--- www/js/appointmentCtrl.js
+++ www/js/appointmentCtrl.js
@@ -1,8 +1,13 @@
-var appointmentCtrl = angular.module('AppointmentCtrl', ['doctorsCtrl']);
+var appointmentCtrl = angular.module('AppointmentCtrl', []);
+//var appointmentCtrl = angular.module('AppointmentCtrl', ['doctorsCtrl']);
+doctorsCtrl.factory('appointmentVO', function(){
+  return {appointmentVO: ""} 
+})
 
-appointmentCtrl.controller('newAppointmentCtrl', function($scope, $ionicModal, Data) {
+
+appointmentCtrl.controller('newAppointmentCtrl', function($scope, $ionicModal, appointmentVO) {
   
-  $scope.data = Data;
+  $scope.appointmentVO = appointmentVO;
 
   // Form data for the Appointments
   $scope.loginData = {};
@@ -10,7 +15,15 @@ appointmentCtrl.controller('newAppointmentCtrl', function($scope, $ionicModal, D
   $ionicModal.fromTemplateUrl('templates/doctorsZoom.html', {
     scope: $scope
   }).then(function(modal) {
-    $scope.modal = modal;
+    $scope.docZmodal = modal;
+  });
+
+
+  // Create the login modal that we will use later
+  $ionicModal.fromTemplateUrl('templates/specialityZoom.html', {
+    scope: $scope
+  }).then(function(modal) {
+    $scope.specZmodal = modal;
   });
 
 
@@ -18,16 +31,25 @@ appointmentCtrl.controller('newAppointmentCtrl', function($scope, $ionicModal, D
 
   // Triggered in the login modal to close it
   $scope.closeZoom = function() {
-    $scope.modal.hide();
+    $scope.docZmodal.hide();
+    $scope.specZmodal.hide();
   };
-  // Open the login modal
-  $scope.doctorZoom = function() {
-    $scope.modal.show();
+  // Open the doctors modal
+  $scope.doctorZoom = function() {    
+    $scope.docZmodal.show();
+  };
+  // Open the specialities modal
+  $scope.specialityZoom = function() {    
+    $scope.specZmodal.show();
   };
 
-  // Open the login modal
+
+  // Set fields on the Appointment form with zoom datas
   $scope.setDoctorChoice = function(doctor) {
-    $scope.data.doctor = doctor.name ;
+    $scope.appointmentVO.doctor = doctor.name;
+  };
+  $scope.setSpecChoice = function(speciality){
+    $scope.appointmentVO.speciality = speciality.description;
   };
 
 
--- www/js/doctorsCtrl.js
+++ www/js/doctorsCtrl.js
@@ -1,33 +1,34 @@
-var doctorsCtrl = angular.module('doctorsCtrl', []);
-doctorsCtrl.factory('Data', function(){
+var doctorsCtrl = angular.module('doctorsCtrl', ['AppointmentCtrl']);
+/*doctorsCtrl.factory('Data', function(){
 	return {doctor: ""} 
-})
+})*/
 
 
 /* Função para busca de Medico para popular zooms.. */
-doctorsCtrl.controller('doctorsSearchZoom', function($scope, Data){
-	$scope.data = Data;
+doctorsCtrl.controller('doctorsSearchZoom', function($scope, appointmentVO){
+	$scope.appointmentVO = appointmentVO;
 
 	// -- Function para trazer os medicos no filtro de busca doctorZoom.htl-- 
-	$scope.doctors = [{id: '345', name: 'Fabiana Aver Pires', specialty: 'Ginecologista', infos: '54 32022522 - Rua os 18 do forte 2000 sala 206'}, 
-                     {id: '543', name: 'Katia Tomaschewski Moitta', specialty: 'Ginecologista', infos: '5430274090 - 5432144094 Rua Feijo Junior, 921AD (Sao Pelegrino)'}, 
-                     {id: '134', name: 'Tirso Luchese Galvan', specialty: 'Ginecologista', infos: '5432212901 - Av. Júlio de Castilhos, 2101 - 78 (Centro)'}, 
-                     {id: '321', name: 'Kenia Fogaça da Silveira', specialty: 'Psiquiatria', infos: '5430390191 - Rua Pinheiro Machado, 2076 sala 304 (Centro)'}, 
-                     {id: '214', name: 'Leonardo Prates de Lima', specialty: 'Psiquiatria', infos: '5432029000 - Rua Moreira César, 2400' }, 
-                     {id: '352', name: 'Aurea dos Santos Celli', specialty: 'Pediatra',  infos: '5430277787 - R Garibaldi, 680 - Sl. 101 (Centro)'}, 
-                     {id: '216', name: 'Alvaro Jose Castilhos', specialty: 'Geriatria', infos: '5432214398 Garibaldi, 789 (Exposicao)'},
-                     {id: '976', name: 'Cristina Worm Weber', specialty: 'Alergista', infos: '5430272929/5432239909 Rua Ernesto Alves, 1887 - sala 602 (Centro)'}, 
-                     {id: '543', name: 'Dagoberto Vanoni de Godoy', specialty: 'Pneumologista', infos: '5432284882 - Rua Arcy Rocha Nóbrega, 401 S 201'}, 
-                     {id: '134', name: 'Deize de Zorzi Piccoli', specialty: 'Gastroenterologista', infos: '5432212901 - Av. Júlio de Castilhos, 2101 - 78 (Centro)'}, 
-                     {id: '321', name: 'Eduardo Gomes De Andrade', epeciality: 'Quiropraxista', infos: '5432194059 - Rua General Arcy da Rocha Nóbrega, 401 (Madureira)'}, 
-                     {id: '214', name: 'Roberto Antonio Conte', specialty: 'Oftalmologista',  infos: '5432282133 - Os Dezoito do Forte, 1098 - Térreo (Sao Pelegrino)'}
+	$scope.doctors = [{id: '345', name: 'Fabiana Aver Pires', speciality: 'Ginecologista', infos: '54 32022522 - Rua os 18 do forte 2000 sala 206'}, 
+                     {id: '543', name: 'Katia Tomaschewski Moitta', speciality: 'Ginecologista', infos: '5430274090 - 5432144094 Rua Feijo Junior, 921AD (Sao Pelegrino)'}, 
+                     {id: '134', name: 'Tirso Luchese Galvan', speciality: 'Ginecologista', infos: '5432212901 - Av. Júlio de Castilhos, 2101 - 78 (Centro)'}, 
+                     {id: '321', name: 'Kenia Fogaça da Silveira', speciality: 'Psiquiatria', infos: '5430390191 - Rua Pinheiro Machado, 2076 sala 304 (Centro)'}, 
+                     {id: '214', name: 'Leonardo Prates de Lima', speciality: 'Psiquiatria', infos: '5432029000 - Rua Moreira César, 2400' }, 
+                     {id: '352', name: 'Aurea dos Santos Celli', speciality: 'Pediatra',  infos: '5430277787 - R Garibaldi, 680 - Sl. 101 (Centro)'}, 
+                     {id: '216', name: 'Alvaro Jose Castilhos', speciality: 'Geriatria', infos: '5432214398 Garibaldi, 789 (Exposicao)'},
+                     {id: '976', name: 'Cristina Worm Weber', speciality: 'Alergista', infos: '5430272929/5432239909 Rua Ernesto Alves, 1887 - sala 602 (Centro)'}, 
+                     {id: '543', name: 'Dagoberto Vanoni de Godoy', speciality: 'Pneumologista', infos: '5432284882 - Rua Arcy Rocha Nóbrega, 401 S 201'}, 
+                     {id: '134', name: 'Deize de Zorzi Piccoli', speciality: 'Gastroenterologista', infos: '5432212901 - Av. Júlio de Castilhos, 2101 - 78 (Centro)'}, 
+                     {id: '321', name: 'Eduardo Gomes De Andrade', speciality: 'Quiropraxista', infos: '5432194059 - Rua General Arcy da Rocha Nóbrega, 401 (Madureira)'}, 
+                     {id: '214', name: 'Roberto Antonio Conte', speciality: 'Oftalmologista',  infos: '5432282133 - Os Dezoito do Forte, 1098 - Térreo (Sao Pelegrino)'}
                     ];
 })
 
 //Realizar busca de especialidades junto ao medico
-doctorsCtrl.controller('specialtySearchZoom', function($scope){
+doctorsCtrl.controller('specialitySearchZoom', function($scope, appointmentVO){
+    $scope.appointmentVO = appointmentVO;
 
-    $scope.specialtys = [{id: '1234', description: 'Ginecologista'},
+    $scope.specialities = [{id: '1234', description: 'Ginecologista'},
                          {id: '4123', description: 'Psiquiatria'},
                          {id: '3454', description: 'Pediatra'},
                          {id: '1853', description: 'Geriatria'},
