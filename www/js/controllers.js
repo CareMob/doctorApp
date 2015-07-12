438623985ce1299920b6fdb92217f29eadab1e50
mtfaoro <mtfaoro@gmail.com>
2015-07-12 00:54:49 -0300

    Verificação de Smartphone
    Verificação de Smartphone

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,8 +1,62 @@
 angular.module('starter.controllers', [])
 
 
-.controller('AppCtrl', function($scope) {
+.controller('AppCtrl', function($scope, $ionicPopup, $http) {
 
+  $scope.validateNumber = function() {
+
+    var appID = '098a8991aefd43f08e8ad8b';
+    var appToken = 'b23389d0d504aa1243de6593721a65f4f6bf8adc';
+    var mobile = '+55' + document.getElementById('txtCellphone').value;
+    var url = 'https://www.cognalys.com/api/v1/otp/?app_id=' + appID
+            + '&access_token=' + appToken
+            + '&mobile=' + mobile;
+
+    $http.get(url).
+    success(function(data, status, headers, config) {
+      if (data.status == 'failed'){
+         var alertPopup = $ionicPopup.alert({
+             title: 'Verificação do Smartphone',
+             template: 'Número informado inválido:' + data.mobile
+          });
+      } else {
+          var alertPopup = $ionicPopup.alert({
+             title: 'Verificação do Smartphone',
+             template: 'Você recebeu uma ligação do número:' + data.otp_start + 'XXXXX. Informe os últimos cinco digitos para validar seu cadastro!' +
+                       '<input type="text"  id="txtCode">'
+          });
+          alertPopup.then(function(res) {
+           url = 'https://www.cognalys.com/api/v1/otp/confirm/?app_id=' + appID
+               + '&access_token=' + appToken
+               + '&keymatch=' + data.keymatch
+               + '&otp=' + data.otp_start + document.getElementById('txtCode').value;
+
+           $http.get(url).
+            success(function(data, status, headers, config) {
+              if (data.status == 'failed'){
+               var alertPopup = $ionicPopup.alert({
+               title: 'Verificação do Smartphone',
+               template: 'Codigo de validação informado está incorreto!' });   
+              } else{
+               var alertPopup = $ionicPopup.alert({
+               title: 'Verificação do Smartphone',
+               template: 'Smartphone verificado com sucesso!' });   
+               }
+
+            }).
+            error(function(data, status, headers, config) {
+            });
+         });
+      }
+    }).
+    error(function(data, status, headers, config) {
+      // called asynchronously if an error occurs
+      // or server returns response with an error status.
+      alert('nok');
+    });
+
+  }
+  
 })
 
 .controller('LoginCtrl', function($scope, $state) {
@@ -11,7 +65,8 @@ angular.module('starter.controllers', [])
   $scope.goToSignIn = function() {
     $state.go('app.signin'); 
   };  
-})
+
+ })
 
 .controller('PlaylistsCtrl', function($scope) {
   $scope.playlists = [
